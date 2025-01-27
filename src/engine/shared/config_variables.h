11116d461a45b4755477c50bcb19855357e9f117
scar17off <scar17off@gmail.com>
2025-01-27 22:30:49 +0200

    Added API key configuration variable

--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -751,3 +751,4 @@ MACRO_CONFIG_INT(ClVideoRecorderFPS, cl_video_recorder_fps, 60, 1, 1000, CFGFLAG
 /*
  * Add config variables for mods below this comment to avoid merge conflicts.
  */
+MACRO_CONFIG_STR(SvApiKey, sv_api_key, 32, "your_api_key_here", CFGFLAG_SERVER, "API key for the website")
